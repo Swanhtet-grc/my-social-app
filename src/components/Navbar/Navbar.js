a4521bb2ebbef6e5a693e235856a61265617a5a0
Swan Htet <swanhtet.grc@gmail.com>
2020-10-16 22:09:21 +0630

    oct16-2

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,7 +4,7 @@ export default function Navbar() {
     return (
         <div>
         <nav className="navbar">
-        <a href="/"><p className="siteName">My Social App</p></a>
+        <a href="/"><p className="siteName">Swan Social App</p></a>
         <a href="/upload"><p className="choices">New Post</p></a>
         <a href="/profile"><p className="choices">My Profile</p></a>
         <a href="/userList"><p className="choices">User List</p></a>
